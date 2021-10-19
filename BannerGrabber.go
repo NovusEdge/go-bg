2686297f55ef70f3fee30b452a6942c0969125ab
NovusEdge <novusedge0@gmail.com>
2021-10-19 10:31:28 -0700

    Only docs remain

---
+++ BannerGrabber.go
@@ -0,0 +1,59 @@
+package gobg
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os/exec"
+)
+
+type BannerGrabber struct {
+	URL string
+}
+
+func (bg *BannerGrabber) Grab() {
+	fmt.Printf("%s[*] Grabbing banner for: %s...\n%s", ColorYellow, bg.URL, ColorReset)
+
+	res, err := curlGrab(bg.URL)
+
+	if err != nil {
+		fmt.Printf("%s[-] E: %s%s\n", ColorRed, err, ColorReset)
+	} else {
+		fmt.Printf("%s[*] Banner Grab Successful!%s\nBanner:\n%s%s%s", ColorYellow, ColorReset, ColorGreen, res, ColorReset)
+	}
+
+}
+
+func (bg *BannerGrabber) Grabs() (string, error) {
+	return res, err
+}
+
+func curlGrab(cURL string, address string) (res string, err error) {
+	cURL, ok := exec.LookPath("curl")
+
+	if !ok {
+		return "", errors.New("E: cURL not found!")
+	}
+
+	fmt.Printf("%s[*] Using cURL to perform the grab%.\n", ColorYellow, ColorReset)
+
+	curlCommand := exec.Command(cURL, "-s", "-I", address)
+
+	stdout, _ := curlCommand.StdoutPipe()
+	stderr, _ := curlCommand.StderrPipe()
+
+	curlCommand.Start()
+
+	outBuf, errBuf := new(bytes.Buffer), new(bytes.Buffer)
+	outBuf.ReadFrom(stdout)
+	errBuf.ReadFrom(stderr)
+
+	res = outBuf.String()
+	errstr := errBuf.String()
+
+	if errstr != "" {
+		return "", errors.New(errstr)
+	}
+
+	return res, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NovusEdge/go-bg
+
+go 1.17
